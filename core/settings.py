6beaaa9cc5b5d31a5aebef3648421fcd7d2f52a2
Wenderson-Oscar <wendersonoscardev@gmail.com>
2023-03-22 08:39:31 -0300

    improving protection of sensitive data

--- core/settings.py
+++ core/settings.py
@@ -12,19 +12,24 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import environs
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environs.Env.read_env(os.path.join(BASE_DIR, '.env'))
+env = environs.Env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv('DEBUG'))
+
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
